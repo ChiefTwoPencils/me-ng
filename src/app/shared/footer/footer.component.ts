96eb436474c581f3e2e60c8f73f62d8886b964ca
ChiefTwoPencils <robertwilk916@gmail.com>
2021-08-25 23:34:35 -0700

    Init commit + stubs for header, footer, and home

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'me-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.sass']
+})
+export class FooterComponent implements OnInit {
+  private _copyYear: number;
+
+  constructor() { 
+    const date = new Date();
+    this._copyYear = date.getFullYear();
+  }
+
+  ngOnInit(): void {
+  }
+
+  get copyYear() {
+    return this._copyYear;
+  }
+
+}
